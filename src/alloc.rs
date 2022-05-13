429021b66f459bf1ca208b4dd52ae0f0c7aa7553
Giacomo Pasini <g.pasini98@gmail.com>
2022-05-13 18:40:34 +0000

    add dummy global alloc

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,14 +1,14 @@
 use crate::{pi_l2_free, pi_l2_malloc};
-use core::alloc::{Allocator, AllocError, Layout};
+use core::alloc::{Allocator, AllocError, Layout, GlobalAlloc};
 use core::ptr::NonNull;
 /// Allocate memory on chip L2 memory
 /// Wrapper around `pi_l2_malloc` and `pi_l2_free`
 const L2_ALIGN: usize = 4;
 
 
-pub struct L2Alloc;
+pub struct L2Allocator;
 
-unsafe impl Allocator for L2Alloc {
+unsafe impl Allocator for L2Allocator {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
         if layout.align() > L2_ALIGN {
             // TODO: use pi_l2_malloc_align()
@@ -20,4 +20,14 @@ unsafe impl Allocator for L2Alloc {
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
         pi_l2_free(ptr.as_ptr() as *mut cty::c_void, layout.size() as i32);
     }
+}
+
+pub struct GlobalAllocator;
+
+unsafe impl GlobalAlloc for GlobalAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        core::ptr::null_mut()
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) { }
+
 }
